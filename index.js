3cc2377f2c2b618eb40325a9d79536fd48b492f1
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-11-08 00:44:24 +1000

    1

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = '5336424335:AAGk0uyo0qqRCrKgvr2J7GrYKK1S0MF8878';
-const webAppUrl = 'https://ornate-selkie-c27577.netlify.app';
+const token = '5654554606:AAHThGKMqmLQhtevrlYvbOj7YDA8PBzX8Aw';
+const webAppUrl = 'https://lively-kelpie-fba5e5.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
